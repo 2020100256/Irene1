9dc3f6324e2cbe7138a4cc163c8214c519745e70
2020100256 <1219415604@qq.com>
2023-04-11 18:44:08 +0800

    test3a

--- app/src/test/java/cn/edu/jnu/x2020100256/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2020100256/BowlingGameUnitTest.java
@@ -33,13 +33,18 @@ public class BowlingGameUnitTest {
 
     @Test
     public void testASpare() {
+        rollASpare();
+        repeatedRoll(0,16);
+        assertEquals(14,game.score());
+    }
+
+    private void rollASpare() {
         game.roll(2);
         game.roll(3);
         game.roll(5);
         game.roll(4);
-        repeatedRoll(0,16);
-        assertEquals(14,game.score());
     }
+
     private void repeatedRoll(int pin, int times) {
         for (int i = 0; i < times; i++) {
             game.roll(pin);
